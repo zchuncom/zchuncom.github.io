cd777cd7e42685ed14932a4c51a510a5c7fe941f
zchuncom <i@zchun.com>
2016-09-04 11:45:23 +0800

    jxy

--- js/jxy.js
+++ js/jxy.js
@@ -2,6 +2,7 @@ if(window.location.href == 'http://www.juxiangyou.com/login/index?redirectUrl=/f
 loginSpeed16();
 } else if(window.location.href == 'http://www.juxiangyou.com/fun/play/speed16/index') {
 speed16uihidden();
+endtime();
 }
 
 function speed16uihidden() {
@@ -15,4 +16,26 @@ function loginSpeed16(){
 $('#account').val('i@zchun.com');
 $('#password').val('21505264');
 $('#code').focus();
+}
+
+
+function endtime() {
+if($(".J_endTime").text() != '') {
+var jz = Date.now() + parseInt($(".J_endTime").text()) * 1000;
+var kj = jz + 10 * 1000;
+
+var jz1 = Math.round((jz - Date.now())/1000);
+var kj1 = jz1 + 10;
+
+
+
+if(top.jz != jz1) {
+try{JS16.endtime(jz1);}catch(e){}
+//console.log('½ØÖÁ:' + jz1 + "/s" + " " + '¿ª½±:' + kj1 + "/s");
+}
+
+top.jz = jz1;
+
+setTimeout(endtime, 100);
+}
 }
