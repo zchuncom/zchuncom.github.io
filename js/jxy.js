b0b0633c38c1da5d2fb12159ce322b9c1b24c106
zchuncom <i@zchun.com>
2016-09-04 11:12:11 +0800

    jxy js16

--- js/jxy.js
+++ js/jxy.js
@@ -1,7 +1,15 @@
 if(window.location.href == 'http://www.juxiangyou.com/login/index?redirectUrl=/fun/play/speed16/index') {
 loginSpeed16();
+} else if(window.location.href == 'http://www.juxiangyou.com/fun/play/speed16/index') {
+speed16uihidden();
 }
 
+function speed16uihidden() {
+$(".fun-header").hide();
+$('.block-right').hide();
+$("#jxy_footer").hide();
+$(".block-main").css("margin", "0 auto").css("float", "none");
+}
 
 function loginSpeed16(){
 $('#account').val('i@zchun.com');
