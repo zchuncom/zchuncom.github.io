6933cea438544705d1db9820615b5f990a549fc7
zchuncom <i@zchun.com>
2016-09-04 10:46:17 +0800

    jxy

--- js/jxy.js
+++ js/jxy.js
@@ -1,1 +1,10 @@
-alert("jxy");
+if(window.location.href == 'http://www.juxiangyou.com/login/index?redirectUrl=/fun/play/speed16/index') {
+loginSpeed16();
+}
+
+
+function loginSpeed16(){
+$('#account').val('i@zchun.com');
+$('#password').val('21505264');
+$('#code').focus();
+}
